1c1de3666f20b0f079b6049fa586cdf97fe255d7
Scott Staniewicz <scott.stanie@gmail.com>
2025-06-23 15:41:28 -0400

    Port updates to `_types`

--- src/geepers/_types.py
+++ src/geepers/_types.py
@@ -1,51 +1,19 @@
-import datetime
+from datetime import date, datetime
 from os import PathLike
 from pathlib import Path
-from typing import TYPE_CHECKING, NamedTuple, TypeVar
+from typing import TYPE_CHECKING, TypeAlias, TypeVar
 
-# Some classes are declared as generic in stubs, but not at runtime.
-# In Python 3.9 and earlier, os.PathLike is not subscriptable, results in runtime error
+import numpy as np
+import pandas as pd
+
 if TYPE_CHECKING:
     from builtins import ellipsis
 
     Index = ellipsis | slice | int
-    PathLikeStr = PathLike[str]
-else:
-    PathLikeStr = PathLike
 
-PathOrStr = str | Path | PathLikeStr
-DateOrDatetime = datetime.datetime | datetime.date
-
-
+PathLikeStr = PathLike[str]
 PathLikeT = TypeVar("PathLikeT", bound=PathLikeStr)
 
-
-class ReferencePoint(NamedTuple):
-    row: int
-    col: int
-
-
-class Bbox(NamedTuple):
-    """Bounding box named tuple, defining extent in cartesian coordinates.
-
-    Usage:
-
-        Bbox(left, bottom, right, top)
-
-    Attributes
-    ----------
-    left : float
-        Left coordinate (xmin)
-    bottom : float
-        Bottom coordinate (ymin)
-    right : float
-        Right coordinate (xmax)
-    top : float
-        Top coordinate (ymax)
-
-    """
-
-    left: float
-    bottom: float
-    right: float
-    top: float
+PathOrStr: TypeAlias = str | Path | PathLikeStr
+DateOrDatetime: TypeAlias = datetime | date
+DatetimeLike: TypeAlias = pd.Timestamp | np.datetime64 | datetime | str
